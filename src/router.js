e554b9f3606a97e90b1ea1c6e0613427a362b8b2
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-22 14:26:46 +0100

    Complete Milestone 1

--- src/router.js
+++ src/router.js
@@ -1,9 +1,37 @@
 import { createRouter, createWebHistory } from "vue-router";
+import AppHome from "./pages/AppHome.vue";
+import AppProjects from "./pages/AppProjects.vue";
+import SingleProject from "./pages/SingleProject.vue";
+import NotFound from "./pages/NotFound.vue";
 
 
 const router = createRouter({
     history: createWebHistory(),
-    routes: [],
+    routes: [
+        {
+            path: "/",
+            name: "home",
+            component: AppHome
+        },
+
+        {
+            path: "/projects",
+            name: "projects",
+            component: AppProjects
+        },
+
+        {
+            path: "/projects/:slug",
+            name: "single-project",
+            component: SingleProject
+        },
+
+        {
+            path: "/:pathMatch(.*)*",
+            name: "not-found",
+            component: NotFound
+          } 
+    ],
 });
 
 export default router;
