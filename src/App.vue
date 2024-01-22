70443db66e658241d88d82a8676b9a737c1f4bb2
albertostizzoli <albertostizzoli@yahoo.it>
2024-01-22 14:16:14 +0100

    update file README

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,20 @@
-<template>
-  <div>
-    <h1>Ciao</h1>
-  </div>
-  <ul>
-    <li v-for="project in projects" :key="project.slug">{{ project.title }}</li>
-  </ul>
-  <button @click="previousPage()">Indietro</button>
-  <button @click="nextPage()">Avanti</button>
+<template> 
+  <router-view></router-view>
 </template>
 
 <script>
-import axios from "axios";
-import { store } from './store.js'
+import { store } from './store.js';
+
 export default {
   name: 'App',
   data() {
     return {
       store,
-      projects: [],
-      currentPage: 1,
-      lastPage: 0
     }
   },
-  methods: {
-    getAllProjects() {
-      axios.get(store.apiUrl + "/projects", { params: { page: this.currentPage } }).then((res) => {
-        console.log(res.data);
-        this.projects = res.data.results.data;
-        console.log(this.projects);
-        this.currentPage = res.data.results.current_page;
-        this.lastPage = res.data.results.last_page;
-      });
-    },
-    nextPage() {
-      if(this.currentPage < this.lastPage){
-        this.currentPage +=1;
-        this.getAllProjects();
-      }
-      
-    },
-    previousPage() {
-      if(this.currentPage > 0){
-        this.currentPage -=1;
-        this.getAllProjects();
-      }
-    }
-  },
-  mounted() {
-    this.getAllProjects();
-  }
-
+  
 }
 </script>
 
-<style lang="scss" scoped></style>
+<style lang="scss" scoped>
+</style>
---
+++ src/pages/AppProjects.vue
@@ -0,0 +1,53 @@
+<template>
+    <div>
+        <ul>
+            <li v-for="project in projects" :key="project.slug">{{ project.title }}</li>
+        </ul>
+        <button @click="previousPage()">Indietro</button>
+        <button @click="nextPage()">Avanti</button>
+    </div>
+</template>
+
+<script>
+import axios from "axios";
+import { store } from '../store.js';
+export default {
+    name: 'AppProjects',
+    data() {
+        return {
+            store,
+            projects: [],
+            currentPage: 1,
+            lastPage: 0,
+        }
+    },
+    methods: {
+        getAllProjects() {
+            axios.get(store.apiUrl + "/projects", { params: { page: this.currentPage } }).then((res) => {
+                console.log(res.data);
+                this.projects = res.data.results.data;
+                console.log(this.projects);
+                this.currentPage = res.data.results.current_page;
+                this.lastPage = res.data.results.last_page;
+            });
+        },
+        nextPage() {
+            if (this.currentPage < this.lastPage) {
+                this.currentPage += 1;
+                this.getAllProjects();
+            }
+        },
+        previousPage() {
+            if (this.currentPage > 0) {
+                this.currentPage -= 1;
+                this.getAllProjects();
+            }
+        }
+    },
+    mounted() {
+        this.getAllProjects();
+    }
+}
+</script>
+
+<style lang="scss" scoped></style>
